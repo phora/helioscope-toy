97b726c30c7c43079fb9bc32be37e54457db450d
Joan Paraiso <joan.paraiso@outlook.com>
2025-07-18 13:20:16 -0700

    Turn on OpenGL 3.2 forward compatibility mode

--- main.cpp
+++ main.cpp
@@ -89,7 +89,7 @@ int main( void )
 	GLFWwindow *window = NULL;
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3 );
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2 );
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
